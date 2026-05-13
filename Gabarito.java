2e3e8824e77aa7d4aaa08f9e42e03b5e986b5c7b
Millene-Oliveira <millene.oliveira.mimi@gmail.com>
2026-05-13 10:49:54 -0300

    Update Gabarito.java

--- Gabarito.java
+++ Gabarito.java
@@ -9,133 +9,141 @@ public class Gabarito {
 		Scanner ler = new Scanner(System.in);
 
 		System.out.println("Deseja descobrir as respostas da prova?(sim/não)");
-		disponibilidade = ler.next();
-		while (disponibilidade.equals("sim")) {
+		temAluno = ler.next();
 
-			System.out.println("É a sua primeira vez por aqui?(sim/não)");
-			temAluno = ler.next();
-			aluno = temAluno.equals("sim") ? aluno + 1 : aluno;
+		while (temAluno.equals("sim")) {
+			aluno++;
+			System.out.println("Olá novo aluno!");
 
-			System.out.println("insira qual questão deseja saber a resposta");
-			questao = ler.nextInt();
-			System.out.println("insira qual a sua resposta(Digite em letra maiúscula)");
-			resposta = ler.next();
+			disponibilidade = "sim";
 
-			switch (questao) {
-			case 1:
-				if (resposta.equals("A")) {
-					System.out.println("Acertou!");
-					acertos++;
-				} else {
-					System.out.println("Errou! A resposta era A");
+			while (disponibilidade.equals("sim")) {
+
+				System.out.println("insira qual questão deseja saber a resposta");
+				questao = ler.nextInt();
+				System.out.println("insira qual a sua resposta(Digite em letra maiúscula)");
+				resposta = ler.next();
+
+				switch (questao) {
+				case 1:
+					if (resposta.equals("A")) {
+						System.out.println("Acertou!");
+						acertos++;
+					} else {
+						System.out.println("Errou! A resposta era A");
+					}
+					System.out.println("Deseja saber a nota de mais alguma questão?");
+					disponibilidade = ler.next();
+					break;
+
+				case 2:
+					if (resposta.equals("B")) {
+						System.out.println("Acertou!");
+						acertos++;
+					} else {
+						System.out.println("Errou! A resposta era B");
+					}
+					System.out.println("Deseja saber a nota de mais alguma questão?");
+					disponibilidade = ler.next();
+					break;
+
+				case 3:
+					if (resposta.equals("C")) {
+						System.out.println("Acertou!");
+						acertos++;
+					} else {
+						System.out.println("Errou! A resposta era C");
+					}
+					System.out.println("Deseja saber a nota de mais alguma questão?");
+					disponibilidade = ler.next();
+					break;
+
+				case 4:
+					if (resposta.equals("D")) {
+						System.out.println("Acertou!");
+						acertos++;
+					} else {
+						System.out.println("Errou! A resposta era 4");
+					}
+					System.out.println("Deseja saber a nota de mais alguma questão?");
+					disponibilidade = ler.next();
+					break;
+
+				case 5:
+					if (resposta.equals("E")) {
+						System.out.println("Acertou!");
+						acertos++;
+					} else {
+						System.out.println("Errou! A resposta era E");
+					}
+					System.out.println("Deseja saber a nota de mais alguma questão?");
+					disponibilidade = ler.next();
+					break;
+
+				case 6:
+					if (resposta.equals("E")) {
+						System.out.println("Acertou!");
+						acertos++;
+					} else {
+						System.out.println("Errou! A resposta era E");
+					}
+					System.out.println("Deseja saber a nota de mais alguma questão?");
+					disponibilidade = ler.next();
+					break;
+
+				case 7:
+					if (resposta.equals("D")) {
+						System.out.println("Acertou!");
+						acertos++;
+					} else {
+						System.out.println("Errou! A resposta era D");
+					}
+					System.out.println("Deseja saber a nota de mais alguma questão?");
+					disponibilidade = ler.next();
+					break;
+
+				case 8:
+					if (resposta.equals("C")) {
+						System.out.println("Acertou!");
+						acertos++;
+					} else {
+						System.out.println("Errou! A resposta era C");
+					}
+					System.out.println("Deseja saber a nota de mais alguma questão?");
+					disponibilidade = ler.next();
+					break;
+
+				case 9:
+					if (resposta.equals("B")) {
+						System.out.println("Acertou!");
+						acertos++;
+					} else {
+						System.out.println("Errou! A resposta era B");
+					}
+					System.out.println("Deseja saber a nota de mais alguma questão?");
+					disponibilidade = ler.next();
+					break;
+
+				case 10:
+					if (resposta.equals("A")) {
+						System.out.println("Acertou!");
+						acertos++;
+					} else {
+						System.out.println("Errou! A resposta era A");
+					}
+					System.out.println("Deseja saber a nota de mais alguma questão?");
+					disponibilidade = ler.next();
+					break;
+
+				default:
+					System.out.println("Resposta inválida. Deseja saber a nota de mais alguma questão?");
+					disponibilidade = ler.next();
 				}
-				System.out.println("Deseja saber a nota de mais alguma questão?");
-				disponibilidade = ler.next();
-				break;
 
-			case 2:
-				if (resposta.equals("B")) {
-					System.out.println("Acertou!");
-					acertos++;
-				} else {
-					System.out.println("Errou! A resposta era B");
-				}
-				System.out.println("Deseja saber a nota de mais alguma questão?");
-				disponibilidade = ler.next();
-				break;
-
-			case 3:
-				if (resposta.equals("C")) {
-					System.out.println("Acertou!");
-					acertos++;
-				} else {
-					System.out.println("Errou! A resposta era C");
-				}
-				System.out.println("Deseja saber a nota de mais alguma questão?");
-				disponibilidade = ler.next();
-				break;
-
-			case 4:
-				if (resposta.equals("D")) {
-					System.out.println("Acertou!");
-					acertos++;
-				} else {
-					System.out.println("Errou! A resposta era 4");
-				}
-				System.out.println("Deseja saber a nota de mais alguma questão?");
-				disponibilidade = ler.next();
-				break;
-
-			case 5:
-				if (resposta.equals("E")) {
-					System.out.println("Acertou!");
-					acertos++;
-				} else {
-					System.out.println("Errou! A resposta era E");
-				}
-				System.out.println("Deseja saber a nota de mais alguma questão?");
-				disponibilidade = ler.next();
-				break;
-
-			case 6:
-				if (resposta.equals("E")) {
-					System.out.println("Acertou!");
-					acertos++;
-				} else {
-					System.out.println("Errou! A resposta era E");
-				}
-				System.out.println("Deseja saber a nota de mais alguma questão?");
-				disponibilidade = ler.next();
-				break;
-
-			case 7:
-				if (resposta.equals("D")) {
-					System.out.println("Acertou!");
-					acertos++;
-				} else {
-					System.out.println("Errou! A resposta era D");
-				}
-				System.out.println("Deseja saber a nota de mais alguma questão?");
-				disponibilidade = ler.next();
-				break;
-
-			case 8:
-				if (resposta.equals("C")) {
-					System.out.println("Acertou!");
-					acertos++;
-				} else {
-					System.out.println("Errou! A resposta era C");
-				}
-				System.out.println("Deseja saber a nota de mais alguma questão?");
-				disponibilidade = ler.next();
-				break;
-
-			case 9:
-				if (resposta.equals("B")) {
-					System.out.println("Acertou!");
-					acertos++;
-				} else {
-					System.out.println("Errou! A resposta era B");
-				}
-				System.out.println("Deseja saber a nota de mais alguma questão?");
-				disponibilidade = ler.next();
-				break;
-
-			case 10:
-				if (resposta.equals("A")) {
-					System.out.println("Acertou!");
-					acertos++;
-				} else {
-					System.out.println("Errou! A resposta era A");
-				}
-				System.out.println("Há outro aluno?");
-				disponibilidade = ler.next();
-				break;
-
-			default:
-				System.out.println("Resposta inválida. Deseja saber a nota de mais alguma questão?");
-				disponibilidade = ler.next();
 			}
+			System.out.println("Há outro aluno?");
+			temAluno = ler.next();
+
 		}
 		ler.close();
 		double media = (double) acertos / aluno;
